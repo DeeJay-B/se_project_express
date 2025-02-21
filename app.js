d8a5d6d40e8340dede035c9dcf4977b402a14a1b
DeeJay-B <dejonbell@gmail.com>
2025-02-20 19:13:43 -0500

    adding schemas

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db)");
-
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
