03de28b055f34426bcd030b54311200a90c16f60
DeeJay-B <dejonbell@gmail.com>
2025-02-21 19:24:53 -0500

    user controller

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", () => console.log("GET items"));
-router.post("/:userId", () => console.log("Post items"));
+router.post("/", () => console.log("Post items"));
 router.delete("/:itemId", () => console.log("deletes items by _id"));
 
 module.exports = router;
